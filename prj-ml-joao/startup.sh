f2c741be26b200445a5894c4e4cedda84a32e2fa
Joao Albertino <j.albertino.neto@gmail.com>
2025-08-21 23:34:33 -0300

    start up

---
+++ prj-ml-joao/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script sets up a Python virtual environment and installs required packages.
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements
+uvicorn app:app --reload
